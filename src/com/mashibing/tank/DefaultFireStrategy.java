9b66fcf027a7fbe21ae1afe34a31bf1704e5ddf2
Zhang <1046035516@qq.com>
2020-08-31 10:13:47 +0800

    装饰类不用了~

--- src/com/mashibing/tank/DefaultFireStrategy.java
+++ src/com/mashibing/tank/DefaultFireStrategy.java
@@ -18,7 +18,7 @@ public class DefaultFireStrategy implements FireStrategy {
     public void fire(Tank t) {
         int bX = t.x + Tank.WIDTH/2 - Bullet.WIDTH/2;
         int bY = t.y + Tank.HEIGHT/2 - Bullet.HEIGHT/2;
-        //TODO:把自己又加了一遍
-        GameModel.getInstance().add(new RectDecorator(new TailDecorator(new Bullet(bX, bY, t.dir,t.getGroup()))));
+//        GameModel.getInstance().add(new RectDecorator(new TailDecorator(new Bullet(bX, bY, t.dir,t.getGroup()))));
+        new Bullet(bX, bY, t.dir,t.getGroup());
     }
 }
