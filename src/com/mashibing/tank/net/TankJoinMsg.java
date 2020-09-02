3cee587986e293552636b4b4ec35c0258e0c1e2c
Zhang <1046035516@qq.com>
2020-09-02 18:20:41 +0800

    上次是错误的，因为test一直通不过，这样就行了，看看有啥区别

---
+++ src/com/mashibing/tank/net/TankJoinMsg.java
@@ -0,0 +1,92 @@
+package com.mashibing.tank.net;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+import com.mashibing.tank.Dir;
+import com.mashibing.tank.Group;
+import com.mashibing.tank.Tank;
+
+/**
+ * @Description TankMsg
+ * @Author Radish
+ * @Date 2020-09-02 11:06
+ */
+public class TankJoinMsg {
+    public int x,y;
+    public Dir dir;
+    public boolean moving;
+    public Group group;
+    public UUID id;
+
+    public TankJoinMsg(Tank t) {
+		this.x = t.getX();
+		this.y = t.getY();
+		this.dir = t.getDir();
+		this.group = t.getGroup();
+		this.id = t.getId();
+		this.moving = t.isMoving();
+	}
+	
+	public TankJoinMsg(int x, int y, Dir dir, boolean moving, Group group, UUID id) {
+		this();
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+		this.moving = moving;
+		this.group = group;
+		this.id = id; 
+	}
+	
+	public TankJoinMsg() {
+	}
+	
+	
+	public byte[] toBytes() {
+		ByteArrayOutputStream baos = null;
+		DataOutputStream dos = null;
+		byte[] bytes = null;
+		try {
+			dos.writeInt(x);
+			dos.writeInt(y);
+			dos.writeInt(dir.ordinal());
+			dos.writeBoolean(moving);
+			dos.writeInt(group.ordinal());
+			dos.writeLong(id.getMostSignificantBits());
+			dos.writeLong(id.getLeastSignificantBits());
+			dos.flush();
+			bytes = baos.toByteArray();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (baos != null) {
+					baos.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				if (dos != null) {
+					dos.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return bytes;		
+	}
+
+	@Override
+	public String toString() {
+		return "TankJoinMsg [x=" + x + ", y=" + y + ", dir=" + dir + ", moving=" + moving + ", group=" + group + ", id="
+				+ id + "]";
+	}
+
+    
+}
